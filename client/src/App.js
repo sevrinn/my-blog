5bd7d09adb06bda93350d81a89c16f867925955f
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-14 19:13:11 -0800

    added navbar and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlePage from './pages/ArticlePage'
+import ArticlesListPage from './pages/ArticlesListPage'
+import NavBar from './NavBar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   const appStyles = 'p-10 bg-sevGrey h-screen'
   return (
-    <div className={appStyles}>
-      <HomePage />
-    </div>
+    <Router>
+      <div className={appStyles}>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} exact />
+          <Route path='/about' element={<AboutPage />} exact />
+          <Route path='/articles-list' element={<ArticlesListPage />} exact />
+          <Route path='/article' element={<ArticlePage />} exact />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
