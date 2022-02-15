a1c8e9d082c38c9d8334ba6d7a4408a2962ae83d
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-14 17:31:35 -0800

    added tailwind

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const HomePage = () => {
+  const h1 = 'text-5xl'
+  const p = 'p-3'
   return (
     <>
-      <h1>Greetings! Welcome to my dev blog</h1>
-      <p>
+      <h1 className={h1}>Greetings! Welcome to my dev blog</h1>
+      <p className={p}>
         This blog is my dedicated area to write about the things I'm currently
         working on in my tech journey.{' '}
       </p>
