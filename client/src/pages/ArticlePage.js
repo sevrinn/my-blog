bf45cc233d3be5ae059bf7aced7a7382d77cbff0
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-14 19:12:45 -0800

    added pages

---
+++ client/src/pages/ArticlePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArticlePage = () => {
+  const h1 = 'text-3xl text-sevYellow'
+  const p = 'p-3 bg-sevPink rounded-md mt-20 w-4/6'
+  return (
+    <div>
+      <h1 className={h1}>Article Page</h1>
+    </div>
+  )
+}
+
+export default ArticlePage
---
+++ client/src/pages/ArticlesListPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArticlesListPage = () => {
+  const h1 = 'text-3xl text-sevYellow'
+  const p = 'p-3 bg-sevPink rounded-md mt-20 w-4/6'
+  return (
+    <div>
+      <h1 className={h1}>ArticlesListPage</h1>
+    </div>
+  )
+}
+
+export default ArticlesListPage
