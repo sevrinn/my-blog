515ed485050af9e778d71d64e841daef681b4fa6
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-14 17:03:06 -0800

    added homepage

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Greetings! Welcome to my dev blog</h1>
+      <p>
+        This blog is my dedicated area to write about the things I'm currently
+        working on in my tech journey.{' '}
+      </p>
+    </>
+  )
+}
+
+export default HomePage
