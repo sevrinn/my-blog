39fe9683d3ba985332771e2d91b0a58e6c89bac4
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-16 12:05:56 -0800

    get article whose name matches name from url param

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import articleContent from './article-content'
 
 const ArticlePage = () => {
-  const h1 = 'text-3xl text-sevYellow'
+  const h1 = 'text-3xl text-sevYellow capitalize'
   const p = 'p-3 bg-sevPink rounded-md mt-20 w-4/6'
 
   const { name } = useParams()
+  const article = articleContent.find((article) => article.name === name)
+
   return (
     <div>
-      <h1 className={h1}>This is the {name} article</h1>
+      <h1 className={h1}>{article.title}</h1>
     </div>
   )
 }
