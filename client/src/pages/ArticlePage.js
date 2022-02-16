86b10e55b4cd1353d18d66edd8bc68b02891fb97
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-16 12:41:03 -0800

    maps article paragraphs to page

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
 import articleContent from './article-content'
 
 const ArticlePage = () => {
   const h1 = 'text-3xl text-sevYellow capitalize'
-  const p = 'p-3 bg-sevPink rounded-md mt-20 w-4/6'
+  const p = 'p-3 bg-sevPink rounded-md mt-20 w-3/4'
 
   const { name } = useParams()
   const article = articleContent.find((article) => article.name === name)
@@ -12,6 +13,11 @@ const ArticlePage = () => {
   return (
     <div>
       <h1 className={h1}>{article.title}</h1>
+      <div className={p}>
+        {article.content.map((paragraph, key) => (
+          <p key={key}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   )
 }
