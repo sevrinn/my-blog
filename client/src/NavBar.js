e40b8d2605b1f0263c666d50092789d7d65613bc
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-16 11:12:37 -0800

    added hover animations for zing

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -5,25 +5,19 @@ const NavBar = () => {
   const navStyle = 'p-3 mb-10'
   const navUlStyle = 'flex justify-end'
   const navLiStyle =
-    'mr-3 p-3 border-2 border-sevBlue rounded-md text-sevBlue text-xl'
-  const activeStyle = 'bg-sevBlue text-sevGrey text-2xl'
+    'transition duration-300 ease-in-out hover:scale-110 hover:bg-sevBlue hover:text-sevGrey mr-3 p-3 border-2 border-sevBlue rounded-md text-sevBlue text-xl'
+
   return (
     <nav className={navStyle}>
       <ul className={navUlStyle}>
-        <li>
-          <NavLink className={navLiStyle} activeStyle={activeStyle} to='/'>
-            Home
-          </NavLink>
+        <li className={navLiStyle}>
+          <NavLink to='/'>Home</NavLink>
         </li>
-        <li>
-          <NavLink className={navLiStyle} to='/about'>
-            About
-          </NavLink>
+        <li className={navLiStyle}>
+          <NavLink to='/about'>About</NavLink>
         </li>
-        <li>
-          <NavLink className={navLiStyle} to='/articles-list'>
-            Entries
-          </NavLink>
+        <li className={navLiStyle}>
+          <NavLink to='/articles-list'>Entries</NavLink>
         </li>
       </ul>
     </nav>
